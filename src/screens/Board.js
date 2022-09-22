c6a38d28440780f82361650f4a16b96a964b141e
Nicolas Ruiz <syngenruiz2@gmail.com>
2022-09-22 16:59:15 -0500

    Redirect board to login if not logged in

--- src/screens/Board.js
+++ src/screens/Board.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { SocialMediaPost } from "../components/index";
 import { logOut } from "../firebase";
 import Button from "react-bootstrap/Button";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const Board = (props) => {
   const handleLogOut = () => {
     logOut();
     window.location.replace("/login");
   };
+
+  const auth = getAuth();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        console.log(`No user detected, Redirect to login`);
+        window.location.replace("/login");
+      }
+    });
+  }, []);
 
   return (
     <div>
